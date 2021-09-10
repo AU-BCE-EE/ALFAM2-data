2c72ee7c453515b83326aaf6ea761ff116d9a21f
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-09-10 11:04:28 -0400

    Intermediate stage, changes for new data uptake

---
+++ scripts/02/main.R
@@ -0,0 +1,50 @@
+# Runs everything necessary for making database from submitted data
+# Creates interval-level and plot-level files for ALFAM2 database
+# This version processes only data from uptake 2 in ALFAM2
+# ALFAM(1) data and uptake 1 data from ALFAM2 are simply read from output in data - ALFAM2 output/01
+
+# To get errors for any warnings
+options(warn = 2) 
+
+# Set database version
+version <- '2.0' # September 2021
+
+# Packages
+library(readxl)
+library(plyr)
+
+# Record versions
+# source('package_versions.R')
+
+# Read in functions
+source('../../functions/02/ALFAM_functions.R')
+
+# Make database files
+sink('../logs/make_database_log.txt')
+  source('make_database.R', echo = TRUE)
+sink()
+
+# Make plot-level file
+source('summaries.R')
+
+# Check for errors in data input or measurements
+sink('../logs/error_log.txt')
+  source('error_checking.R', echo = TRUE)
+sink()
+
+# Compare to older version
+sink('../logs/version_comparison.txt')
+  cat('Compare to original version \n')
+  source('compare_1.0.R', echo = TRUE)
+  cat('Compare to previous version \n')
+  source('compare_prev.R', echo = TRUE)
+sink()
+
+# Remove partial file
+file.remove('../data - ALFAM2 output/ALFAM_part.csv')
+
+options(warn = 1)
+
+cat('Note to SASHA: !!!!!!!!!!!!!!!!!!!!!!!!!!1 \n 
+    Remember to create a tag/release on GitHub!!! \n
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
