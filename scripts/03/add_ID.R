44dc0b7475f721aa402dba26b5d1c92c9e94cf55
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-02-01 15:15:49 -0500

    Progress toward merge of new data

---
+++ scripts/03/add_ID.R
@@ -0,0 +1,20 @@
+# Add plot and institute ID codes to new data
+
+# Extract and reuse old institute codes
+inst.old <- unique(pdat.old[, c('institute', 'inst')])
+pdat <- merge(pdat, inst.old, by = 'institute', all.x = TRUE)
+
+# Create completley new 300s codes for new institutes
+# NTS: needs tweak to leave out inst codes already recognized
+pdat$inst[is.na(pdat$inst)] <- 300 + as.integer(factor(pdat$institute))[is.na(pdat$inst)]
+
+# ID codes created in plots data frame and then merged into interval level data frame
+# Experiment ID
+pdat$eid <- as.integer(factor(pdat$ceid)) + max(pdat.old$eid)
+# Add plot and plot x meas tech IDs
+pdat$pid <- as.integer(factor(pdat$cpid)) + max(pdat.old$pid)
+pdat$pmid <- as.integer(factor(pdat$cpmid)) + max(pdat.old$pmid)
+
+# Merge into interval level data
+idat <- merge(idat, pdat[, c('institute', 'file', 'exper', 'inst', 'eid')], by = c('institute', 'file', 'exper'), all.x = TRUE)
+
