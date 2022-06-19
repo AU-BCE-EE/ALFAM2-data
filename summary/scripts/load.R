79ccef1e64b3fa71a0e60565575473cb058ab91c
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-06-19 18:10:18 -0400

    Add data summary dir

---
+++ summary/scripts/load.R
@@ -0,0 +1,4 @@
+
+# Note that fread requires R.utils
+idat <- as.data.frame(fread('../../../ALFAM2-data/data-output/03/ALFAM2_interval.csv.gz'))
+pdat <- as.data.frame(fread('../../../ALFAM2-data/data-output/03/ALFAM2_plot.csv.gz'))
