752b07d9fb4c9a9b6158847e3fa4656481750f8f
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-07-04 16:31:17 -0400

    Reorganize

--- summary/scripts/load.R
+++
@@ -1,4 +0,0 @@
-
-# Note that fread requires R.utils
-idat <- as.data.frame(fread('../../../ALFAM2-data/data-output/03/ALFAM2_interval.csv.gz'))
-pdat <- as.data.frame(fread('../../../ALFAM2-data/data-output/03/ALFAM2_plot.csv.gz'))
