f2c7ee68eb819a8b9d957943e30ff6cae399cf38
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-09-20 06:14:43 -0400

    Add database version files

--- scripts/03/export.R
+++ scripts/03/export.R
@@ -6,3 +6,7 @@ write.csv(idat.comb, '../../data-output/03/ALFAM2_interval.csv', row.names = FAL
 # And then compress them
 R.utils::gzip('../../data-output/03/ALFAM2_plot.csv', overwrite = TRUE)
 R.utils::gzip('../../data-output/03/ALFAM2_interval.csv', overwrite = TRUE)
+
+# Record data version also
+cat(paste0('Database version: ', version, '. Date created: ', Sys.time()), file = '../../logs/03/data_version.txt')
+cat(paste0('Database version: ', version, '. Date created: ', Sys.time()), file = '../../data-output/03/data_version.txt')
