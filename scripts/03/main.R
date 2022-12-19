ea2839934728865ddf01acc39722051fda962291
Sasha D. Hafner <sasha@hafnerconsulting.com>
2022-12-19 17:20:33 -0500

    rm duplicate html logs with double __, v2.15

--- scripts/03/main.R
+++ scripts/03/main.R
@@ -11,7 +11,7 @@ rm(list = ls())
 
 # Set uptake number and database version
 uptake <- 3
-version <- '2.14' # Dec 2022
+version <- '2.15' # Dec 2022
 
 # Create all html log files (otherwise just missing ones)?
 alllogs <- FALSE
