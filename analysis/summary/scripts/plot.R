d5e3546b1e9bf5e8dc8aa87833ca7741e1f31189
Sasha Hafner <35272876+sashahafner@users.noreply.github.com>
2023-03-27 14:20:02 -0400

    tweak for paper

--- analysis/summary/scripts/plot.R
+++ analysis/summary/scripts/plot.R
@@ -7,10 +7,11 @@ ggplot(dd, aes(country, e.rel.final, colour = app.mthd, shape = app.mthd)) +
   geom_jitter(alpha = 0.6) +
   ylim(0, 1.5) +
   scale_color_brewer(palette = "Set1") +
-  labs(x = 'Country', y = 'Final relative emission (% applied TAN)', colour = '', shape = '') +
+  labs(x = 'Country', y = 'Relative emission (% TAN)', colour = '', shape = '') +
   theme(legend.position = 'top', axis.text.x = element_text(angle=90, vjust=.5, hjust=1)) +
   facet_wrap(~ paste('Uptake', uptake)) 
 ggsave('../plots/ALFAM2_data_summary.pdf', height = 3.3, width = 6.7)
+ggsave('../plots/ALFAM2_data_summary.png', height = 3.3, width = 4)
 
 dd <- subset(cdat, app.method %in% c('bc', 'bsth', 'ts', 'os', 'cs'))
 dd$app.mthd <- factor(dd$app.method, levels = c('bc', 'bsth', 'ts', 'os', 'cs'), 
