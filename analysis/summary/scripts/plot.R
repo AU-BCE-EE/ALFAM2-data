e7ac2c1b9d3e53694bb68a00bfc5a7e27040cee7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-27 14:35:04 -0400

    Tweak for paper

--- analysis/summary/scripts/plot.R
+++ analysis/summary/scripts/plot.R
@@ -5,13 +5,15 @@ dd$app.mthd <- factor(dd$app.method, levels = c('bc', 'bsth', 'ts', 'os', 'cs'),
                       labels = c('Broadcast', 'Trailing hose', 'Trailing shoe', 'Open slot injection', 'Closed slot injection'))
 ggplot(dd, aes(country, e.rel.final, colour = app.mthd, shape = app.mthd)) +
   geom_jitter(alpha = 0.6) +
-  ylim(0, 1.5) +
   scale_color_brewer(palette = "Set1") +
   labs(x = 'Country', y = 'Relative emission (% TAN)', colour = '', shape = '') +
+  theme_bw() +
   theme(legend.position = 'top', axis.text.x = element_text(angle=90, vjust=.5, hjust=1)) +
-  facet_wrap(~ paste('Uptake', uptake)) 
+  facet_wrap(~ paste('Uptake', uptake)) +
+  coord_cartesian(ylim = c(0, 1.3)) +
+  guides(colour = guide_legend(nrow = 2))
 ggsave('../plots/ALFAM2_data_summary.pdf', height = 3.3, width = 6.7)
-ggsave('../plots/ALFAM2_data_summary.png', height = 3.3, width = 4)
+ggsave('../plots/ALFAM2_data_summary.png', height = 3.0, width = 5)
 
 dd <- subset(cdat, app.method %in% c('bc', 'bsth', 'ts', 'os', 'cs'))
 dd$app.mthd <- factor(dd$app.method, levels = c('bc', 'bsth', 'ts', 'os', 'cs'), 
