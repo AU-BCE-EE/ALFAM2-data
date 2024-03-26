50d89e639d9b6394fdb59d5d04a9eff712ca1f38
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-03-26 11:22:50 -0400

    Edit WUR data extraction based on Jan email

--- data-submitted/03/WUR/data-prep/scripts/merge_arable.R
+++ data-submitted/03/WUR/data-prep/scripts/merge_arable.R
@@ -32,35 +32,44 @@ arablep[, man_DM := dm]
 arablep[, `:=` (man_VS = '', man_TN = '', man_TAN = concNH4, man_TIC = '', man_UA = '', man_VFA = '')]
 arablep[, man_pH := ph]
 arablep[, `:=` (app_start = '', app_end = '')]
+table(arablep$app_method)
 arablep[, app_method := method]
 arablep[grepl('[Bb]roadcast', app_method), app_method := 'Broadcast']
+# Deep placement may be closed slot injection or surface application followed by deep incorporation
+# Next several lines sort this, with some overriding of lines above
 arablep[grepl('[Dd]eep placement', app_method), app_method := 'Closed slot injection']
 arablep[grepl('[Ss]hallow[Ii]njection', app_method), app_method := 'Open slot injection']
-# NTS: Check with Jan, there 
-arablep[grepl('NarrowBand', app_method), app_method := 'Trailing hose']
+arablep[grepl('NarrowBand (hose)', app_method), app_method := 'Trailing hose']
+arablep[grepl('NarrowBand', app_method), app_method := 'Trailing shoe']
+# Take any "mouldboard plough" in methodDetail as broadcast application
+# This will override some of the os values that were based on "deep placement" in method above!
+arablep[grepl('[Mm]ouldboard plough', app_incorp_method), app_method := 'Broadcast']
+
 arablep[, app_rate := rate]
 arablep[, app_unit := 't/ha']
-# NTS: check incorp interpretation!
+
+# Incorporation
 arablep[, incorp := 'None']
 arablep[grepl('[Ii]ncorp|[Cc]ultiv', app_incorp_method), incorp := 'Shallow']
 arablep[grepl('[Pp]lough', app_incorp_method), incorp := 'Deep']
-# Take any "mouldboard plough" in methodDetail as broadcast application
-# This will override some of the os values that were based on "deep placement" in method above!
-# NTS: Is this correct? Waiting on Jan's answer. . .
-arablep[grepl('[Mm]ouldboard plough', app_incorp_method), app_method := 'Broadcast']
-# NTS: And incorporation time!
-arablep[, incorp_time := 0]
-arablep[incorp != 'None', incorp_time := 0.1]
+
+# Assume incorporation delay of 3 min based on email from Jan ("few minutes")
+arablep[, incorp_time := 0.05]
+arablep[grepl('0.5 hrs delay', methodDetail), incorp_time := 0.5]
+arablep[grepl('1.5 hrs delay', methodDetail), incorp_time := 1.5]
 arablep[incorp == 'None', incorp_time := NA]
 arablep[, `:=` (man_area = '', inj_dist = '', fur_depth = '', fur_width = '')]
 # Sort out crop--this info is in multiple columns in original data
 arablep[, crop := 'None']
 arablep[landuse != 'baresoil', crop := landuse]
+# Note there are a few rows interpreted as stubble because of info column, even wuth stubble column has no height value
 arablep[!is.na(stubble) & stubble > 0, crop := 'stubble']
 arablep[grepl('stubble', info), crop := 'stubble']
 arablep[, crop_height := stubble]
 
 # Plot sheet
+# Note that I lazily rely on column creation order to get final order correct
+# Not so for emission sheet
 arableplot <- arablep[, project:crop_height]
 
 # Experments and Treatments worksheets are subsets of Plots here (because notes are missing there is nothing more to include)????
@@ -70,27 +79,34 @@ arabletreat <- arablep[, .(project, experiment, plot_code, treatment_descrip)]
 
 # Emission sheet
 # Fix id problem from one sheet
-arable3[, id := gsub("^'", '', id, 'B')]
-arable3[, experiment := id2exper(id, 'B')]
+arable4[, id := gsub("^'", '', id, 'B')]
 arable4[, experiment := id2exper(id, 'B')]
+arable5[, experiment := id2exper(id, 'B')]
 # Merge by experiment not id, because of missing values
 # Get more than 973 rows when merging on time and timeCum, so omitted here
-arablee <- merge(arable3, arable4, by = c('nr', 'experiment', 'shift'), suffixes = c('.3', '.4'), all = TRUE)
+arablee <- merge(arable4, arable5, by = c('nr', 'experiment', 'shift'), suffixes = c('.3', '.4'), all = TRUE)
 arablee <- merge(arablee, arablep, by = c('nr', 'experiment', 'year', 'method'))
+
+# Get interval timing from nData
+arable3[, experiment := id2exper(id, 'B')]
+arablee <- merge(arablee, arable3[, .(nr, experiment, shift, start, end)], by = c('nr', 'experiment', 'shift'))
 
 # Average interval flux calculations
 arablee[, flux := percEXP / 100 * NH4KgHa / time.4]
 
+# Convert radiation from J/cm2-h to J/m2-s
+arablee[, rad := radiation * 10000 / (time.4 * 3600)]
+
 # Emission sheet
-arablee[, `:=` (t_start = '', t_end = '')]
+arablee[, `:=` (t_start = as.POSIXct(start, format = '%d-%m-%Y %H:%M:%S'), t_end = as.POSIXct(end, format = '%d-%m-%Y %H:%M:%S'))]
 arablee[, meas_tech := 'IHF']
 arablee[, meas_tech_det := 'IHF']
 arablee[, `:=` (detect_lim = '', d_val = '', d_unit = '')]
 arablee[, flux_unit := 'kg N/ha-hr']
 arablee[, surf_pH := '']
-arablee[, air_temp_z := 10]
+arablee[, air_temp_z := 1.5]
 arablee[, `:=` (soil_temp = '', soil_temp_z = '', soil_surf_temp = '')]
 arablee[, wind_z := 2]
 arableemis <- arablee[, .(project, experiment, field, plot_code, plot_code, replicate, shift, t_start, t_end, time.4,
                           meas_tech, meas_tech_det, detect_lim, d_val, d_unit, flux, flux_unit, surf_pH, temp,
-                          air_temp_z, soil_temp, soil_temp_z, soil_surf_temp, radiation, wind2m, wind_z)] 
+                          air_temp_z, soil_temp, soil_temp_z, soil_surf_temp, rad, wind2m, wind_z)] 
--- data-submitted/03/WUR/data-prep/scripts/merge_grass.R
+++ data-submitted/03/WUR/data-prep/scripts/merge_grass.R
@@ -8,10 +8,8 @@ grassp[, treatment_descrip := paste(method, treatment, info)]
 # Sort out names and units
 # NTS: Note gsub '-' to deal with e.g., 2014-23a vs. without a
 grassp[, project := id2exper(id, 'G')]
-# NTS copy to arable???
 grassp[, experiment := id2exper(id, 'G')]
 grassp[, field := paste0(ifelse(!is.na(idField), paste0(idField, '-'), ''), location)]
-# NTS copy to arable???
 grassp[, plot_code := gsub('-', '', substr(id, 9, 12))]
 grassp[, replicate := '']
 grassp[, size := area]
@@ -39,11 +37,9 @@ grassp[, app_method := method]
 grassp[grepl('[Bb]roadcast', app_method), app_method := 'Broadcast']
 grassp[grepl('[Dd]eep placement', app_method), app_method := 'Closed slot injection']
 grassp[grepl('[Ss]hallow[Ii]njection', app_method), app_method := 'Open slot injection']
-# NTS: check next with Jan
-grassp[grepl('[Nn]arrow[Bb]and', app_method), app_method := 'Trailing hose']
+grassp[grepl('[Nn]arrow[Bb]and', app_method), app_method := 'Trailing shoe']
 grassp[, app_rate := rate]
 grassp[, app_unit := 't/ha']
-# NTS: check no incorporation!
 grassp[, incorp := 'None']
 grassp[, incorp_time := '']
 grassp[, `:=` (man_area = '', inj_dist = '', fur_depth = '', fur_width = '')]
@@ -60,30 +56,36 @@ grasstreat <- grassp[, .(project, experiment, plot_code, treatment_descrip)]
 
 # Emission sheet
 # Fix id problem from one sheet
-grass3[, id := gsub("^'", '', id)]
-# NTS: fix in arable too!
-grass3[, experiment := id2exper(id, 'G')]
+grass4[, id := gsub("^'", '', id)]
 grass4[, experiment := id2exper(id, 'G')]
-##grass3[, experiment := paste0('G', substr(id, 1, 7))]
-##grass4[, experiment := paste0('G', substr(id, 1, 7))]
+grass5[, experiment := id2exper(id, 'G')]
+
 # Merge by experiment not id, because of missing values
 # Get more than 973 rows when merging on time and timeCum, so omitted here
-grasse <- merge(grass3, grass4, by = c('nr', 'experiment', 'shift'), suffixes = c('.3', '.4'), all = TRUE)
+grasse <- merge(grass4, grass5, by = c('nr', 'experiment', 'shift'), suffixes = c('.3', '.4'), all = TRUE)
 grasse <- merge(grasse, grassp, by = c('nr', 'experiment', 'year', 'method'))
+# Get interval timing from nData
+grass3[, experiment := id2exper(id, 'G')]
+
+# Emission sheet
+grasse <- merge(grasse, grass3[, .(nr, experiment, shift, start, end)], by = c('nr', 'experiment', 'shift'))
+
+grasse[, `:=` (t_start = as.POSIXct(start, format = '%d-%m-%Y %H:%M:%S'), t_end = as.POSIXct(end, format = '%d-%m-%Y %H:%M:%S'))]
 
 # Average interval flux calculations
 grasse[, flux := percEXP / 100 * NH4KgHa / time.4]
 
-# Emission sheet
-grasse[, `:=` (t_start = '', t_end = '')]
+# Convert radiation from J/cm2-h to J/m2-s
+grasse[, rad := radiation * 10000 / (time.4 * 3600)]
+
 grasse[, meas_tech := 'IHF']
 grasse[, meas_tech_det := 'IHF']
 grasse[, `:=` (detect_lim = '', d_val = '', d_unit = '')]
 grasse[, flux_unit := 'kg N/ha-hr']
 grasse[, surf_pH := '']
-grasse[, air_temp_z := 10]
+grasse[, air_temp_z := 1.5]
 grasse[, `:=` (soil_temp = '', soil_temp_z = '', soil_surf_temp = '')]
 grasse[, wind_z := 2]
 grassemis <- grasse[, .(project, experiment, field, plot_code, plot_code, replicate, shift, t_start, t_end, time.4,
                           meas_tech, meas_tech_det, detect_lim, d_val, d_unit, flux, flux_unit, surf_pH, temp,
-                          air_temp_z, soil_temp, soil_temp_z, soil_surf_temp, radiation, wind2m, wind_z)] 
+                          air_temp_z, soil_temp, soil_temp_z, soil_surf_temp, rad, wind2m, wind_z)] 
