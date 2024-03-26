a355290473aa33cfe0734c713a80b8fd0509daf7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-03-26 14:27:24 -0400

    rm out old out of place files

--- scripts/02/load.R
+++
@@ -1,7 +0,0 @@
-# Loads data for analysis
-
-source('../functions/ALFAM_functions.R')
-
-d <- read.csv('../data/ALFAM_interval.csv')
-ds <- read.csv('../data/ALFAM_plot.csv')
-dsn <- read.csv('../data/ALFAM_plot_norm.csv')
