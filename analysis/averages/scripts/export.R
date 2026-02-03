af0654817f633833c8bd52d407e2cb7dceb1e5a7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-02-03 11:25:59 -0500

    copy database ver file with averages

--- analysis/averages/scripts/export.R
+++ analysis/averages/scripts/export.R
@@ -2,3 +2,7 @@
 fwrite(cdat, '../output/country_means.csv')
 fwrite(sdat, '../output/source_means.csv')
 fwrite(odat, '../output/overall_means.csv')
+
+file.copy('../../../data-output/03/data_version.txt', '../output/data_version.txt')
+file.copy('../../../data-output/03/data_version.txt', '../logs/data_version.txt')
+
