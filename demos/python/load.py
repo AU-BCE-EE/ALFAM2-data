2f3314fd3ea17503655aaf1164444f0ac819478c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-03-17 09:30:35 -0400

    rm demos dir.
    Info is repeated in README.md

--- demos/python/load.py
+++
@@ -1,6 +0,0 @@
-# Example calls for reading ALFAM2 data using Python
-
-import pandas as pd
-
-idat = pd.read_csv('../../../ALFAM2-data/data-output/03/ALFAM2_interval.csv.gz')
-pdat = pd.read_csv('../../../ALFAM2-data/data-output/03/ALFAM2_plot.csv.gz')
